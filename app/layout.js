b00449cd6822df60780ed373783a0d728e7d3d26
Brunn09 <brunnobatista25@gmail.com>
2023-05-25 21:25:50 -0300

    First api request

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
-import NavbarFooter from './components/navbar-footer'
+import Navbar from './components/navbar'
+import Footer from './components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,11 +16,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
       <body className={inter.className}>
-        <NavbarFooter>
+        <Navbar />
           {children}
-        </NavbarFooter>
+        <Footer />
       </body>
     </html>
   )
--- app/page.js
+++ app/page.js
@@ -1,10 +1,35 @@
-
+import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
+async function getData() {
+  const data = await fetch("https://pokeapi.co/api/v2/pokemon?limit=90&offset=0").then((res) => res.json())
+
+  const results = data.results
+  const PkName = results[0].name
+  
+  
+  const moreInfo = results[0].url
+  const info = await fetch(moreInfo).then((res) => res.json())
+  
+  const img = info.sprites.versions['generation-v']['black-white'].animated.front_default
+
+  
+  return {
+    'name': PkName,
+    'img': img
+  }
+}
+
+export default async function Home() {
+
+  const pokemon = await getData()  
+
   return (
     <main className={styles.container}>
-      <h1>Poke Next</h1>
+      <h1>{pokemon.name}</h1>
+      <Image src={pokemon.img} alt={pokemon.name} height={100} width={100} loading='lazy'/>
     </main>
   )
 }
+
+/*https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/2.gif*/
