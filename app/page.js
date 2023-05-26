3d40afb8537d3452fd8d6d87b0b26f396e49dbe5
Brunn09 <brunnobatista25@gmail.com>
2023-05-26 20:00:21 -0300

    Card for pokemon created

--- app/page.js
+++ app/page.js
@@ -2,33 +2,46 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 async function getData() {
-  const data = await fetch("https://pokeapi.co/api/v2/pokemon?limit=90&offset=0").then((res) => res.json())
+  const data = await fetch("https://pokeapi.co/api/v2/pokemon?limit=30&offset=0").then((res) => res.json())
 
   const results = data.results
-  const PkName = results[0].name
-  
-  
-  const moreInfo = results[0].url
-  const info = await fetch(moreInfo).then((res) => res.json())
-  
-  const img = info.sprites.versions['generation-v']['black-white'].animated.front_default
 
-  
-  return {
-    'name': PkName,
-    'img': img
+  let pokemons = []
+
+  for (let i = 0; i < results.length; i++) {
+    const PkName = results[i].name
+
+    const moreInfo = results[i].url
+    const info = await fetch(moreInfo).then((res) => res.json())
+
+    const img = info.sprites.versions['generation-v']['black-white'].animated.front_default
+
+    pokemons[i] = {
+      name: PkName,
+      image: img
+    }
   }
+
+  return pokemons
 }
 
 export default async function Home() {
 
-  const pokemon = await getData()  
+  const pokemons = await getData()
 
   return (
-    <main className={styles.container}>
-      <h1>{pokemon.name}</h1>
-      <Image src={pokemon.img} alt={pokemon.name} height={100} width={100} loading='lazy'/>
-    </main>
+    <div className={styles.container}>
+      <div className={styles.pkCards}>
+        {pokemons.map((pokemon) => (
+          <div className={styles.card}>
+            <h1>{pokemon.name}</h1>
+            <Image src={pokemon.image} alt={pokemon.name} height={100} width={100} loading='lazy' />
+
+            <p className={styles.button}>details</p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
